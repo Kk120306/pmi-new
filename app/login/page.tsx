294a016b5269ac5404ce8a92567145e8885bc5bc
Kai Kameyama <1203kk06@gmail.com>
2025-06-25 13:19:24 +0800

    remove for now

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,10 @@
-import { LogInForm } from "@/app/ui/login/LogInForm";
+// import { LogInForm } from "@/app/ui/login/LogInForm";
 
-export default function LoginPage() {
-  return (
-    <main>
-      <h1>Sign In</h1>
-      <LogInForm />
-    </main>
-  )
-}
+// export default function LoginPage() {
+//   return (
+//     <main>
+//       <h1>Sign In</h1>
+//       <LogInForm />
+//     </main>
+//   )
+// }
